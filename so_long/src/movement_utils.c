8e4f186c2f98d264a7cd84da17a07702b3b8fa6b
Luís Quental Almeida <luis.quental.almeida@gmail.com>
2026-01-22 07:29:36 +0000

    Add final movement_utils.c version
    Add final movement_utils.c version, submitted for evaluation. File contains utils functions for movement: check for allowed movement, increment collectible count and switch to floor texture, and check for allowed exit.

---
+++ so_long/src/movement_utils.c
@@ -0,0 +1,31 @@
+#include "so_long.h"
+
+int	next_blocked(t_game *game, int nxt_i, int nxt_j)
+{
+	if (nxt_i < 0 || nxt_j < 0)
+		return (1);
+	if (nxt_i >= game->map.width || nxt_j >= game->map.height)
+		return (1);
+	if (game->map.grid[nxt_j][nxt_i] == '1')
+		return (1);
+	return (0);
+}
+
+void	get_collectible(t_game *game, int nxt_i, int nxt_j)
+{
+	game->c_count++;
+	game->map.grid[nxt_j][nxt_i] = '0';
+}
+
+int	can_exit(t_game *game, char cell)
+{
+	if (cell != 'E')
+		return (0);
+	if (game->c_count == game->c_total)
+	{
+		game->mv_count++;
+		ft_printf("Current move count: %d moves.\n", game->mv_count);
+		handle_close(game);
+	}
+	return (0);
+}
